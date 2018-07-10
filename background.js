4c264902ab93e5ea5acf984e135c2c8aad460209
Anton Smorodskyi <asmorodskyi@suse.com>
2018-07-10 14:02:25 +0200

    Implement querying of openQA

--- background.js
+++ background.js
@@ -2,7 +2,26 @@ var jobIds = [];
 
 chrome.alarms.onAlarm.addListener(function (alarm) {
     for (var key in jobIds) {
-        console.log("Processing job with id=" + jobIds[key].href);
+        var xhr = new XMLHttpRequest();
+        var URLString = jobIds[key].origin+"/api/v1/jobs/"+key.substring(key.lastIndexOf("/")+1,key.length);
+        console.log("Requesting URL-"+ URLString);
+        var requestURL = new URL(URLString);
+        xhr.onreadystatechange = function() {
+            if(this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+                var jobJSON = JSON.parse(this.responseText);
+                if(jobJSON["job"]["state"] === "done" || jobJSON["job"]["state"] === "canceled") {
+                    console.log("Job " + key + " is done or canceled ");
+                    delete jobIds[key];
+                    new Notification('openQAChecker', {
+                        icon: '48.png',
+                        body: 'Job ' + key + "is finished"
+                      });
+                }
+                console.log("Current job state - " + jobJSON["job"]["state"]);
+            }
+        }
+        xhr.open("GET", requestURL, true);
+        xhr.send();
     }
 });
 
