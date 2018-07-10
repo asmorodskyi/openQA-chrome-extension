67476348dbbeeb3a1efc82421f2d8f0e9d89d686
Anton Smorodskyi <asmorodskyi@suse.com>
2018-07-10 11:54:56 +0200

    Ability to add openQA jobs in queue for processing
    1. Single alarm set to process all jobs
    2. Clear Alarm functionality droped for now
    3. Unique array of jobs to process

--- background.js
+++ background.js
@@ -1,19 +1,44 @@
-chrome.alarms.onAlarm.addListener(function( alarm ) {
-    console.log("Got an alarm!", alarm);
-  });
+var jobIds = [];
 
-  chrome.runtime.onMessage.addListener( function(request,sender,sendResponse)
-{
-    if( request.greeting === "GetURL" )
-    {
-        var tabURL = "Not set yet";
-        chrome.tabs.query({active:true},function(tabs){
-            if(tabs.length === 0) {
-                sendResponse({});
-                return;
+chrome.alarms.onAlarm.addListener(function (alarm) {
+    for (var key in jobIds) {
+        console.log("Processing job with id=" + jobIds[key].href);
+    }
+});
+
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.alarms.get("openQAChecker", function(alarm) {
+        if(alarm === undefined ){
+            console.log("First pluging start. Creating alarm");
+            chrome.alarms.create("openQAChecker", {periodInMinutes: 0.1});
+        }
+        else{
+            console.log("Alarm already exists");
+        }
+    });
+})
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.messageid === "CreateAlarm") {
+        chrome.tabs.query({active: true}, function (tabs) {
+            if (tabs.length !== 0 && /\/tests\/\d{1,7}/.test(tabs[0].url)) {
+                var exists = false;
+                for(var key in jobIds){
+                    if(key === tabs[0].url) {
+                        exists=true;
+                        break;
+                    }
+                }
+                if(exists) {
+                    console.log("Already processing "+ tabs[0].url)
+                }else {
+                    var url = new URL(tabs[0].url);
+                    console.log('Start tracking ' + url.pathname + " for host - " + url.hostname);
+                    jobIds[tabs[0].url]=url;
+                }
+            } else {
+                console.log('Wrong tab');
             }
-            tabURL = tabs[0].url;
-            sendResponse( {navURL:tabURL} );
-        });        
+        });
     }
 });
--- popup.js
+++ popup.js
@@ -1,20 +1,6 @@
 function setAlarm(event) {
-    var tabURL='Undef';
-    chrome.runtime.sendMessage({greeting: "GetURL"},
-        function (response) {
-            tabURL = response.navURL;
-            alert(tabURL);
-            chrome.alarms.create("alarmName", {delayInMinutes: 1});
-        });
+    chrome.runtime.sendMessage({messageid: "CreateAlarm"});
     window.close();
-  }
-  
-  function clearAlarm() {
-    chrome.alarms.clearAll();
-    window.close();
-  }
-  
-  //An Alarm delay of less than the minimum 1 minute will fire
-  // in approximately 1 minute incriments if released
-  document.getElementById('setAlarm').addEventListener('click', setAlarm);
-  document.getElementById('clearAlarm').addEventListener('click', clearAlarm);
+}
+
+document.getElementById('setAlarm').addEventListener('click', setAlarm);
